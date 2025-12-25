fa32150701fa66feb35e41ee60ed950925c9c9c9
TheSilentIce <srikaryadlapati@gmail.com>
2025-12-25 11:44:30 -0800

    added refferences

--- Program/genetic_util.h
+++ Program/genetic_util.h
@@ -8,9 +8,9 @@ std::vector<float> simulated_binary_crossover(Portfolio *parent1,
                                               bool positive);
 std::vector<float> blend_crossover(Portfolio *parent1, Portfolio *parent2);
 
-void mutate(std::vector<float> child);
+void mutate(std::vector<float> &child);
 void normalize(std::vector<float> &child);
 
-Portfolio create_child(std::vector<float> child);
+Portfolio create_child(std::vector<float> &child);
 Portfolio *create_random_portfolio(const std::vector<std::string> &keys);
 #endif
